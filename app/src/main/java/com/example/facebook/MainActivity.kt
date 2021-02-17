72b38cbf9e88032a2539114c94f3b943ab781360
anshul <lci2020020@iiitl.ac.in>
2021-02-17 18:48:58 +0530

    This is first committ

---
+++ app/src/main/java/com/example/facebook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val img=findViewById<ImageView>(R.id.fb)
+        img.setImageResource(R.drawable.fbicon)
+
+
+
+
+    }
+}
